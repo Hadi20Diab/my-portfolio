39574771dcc65c5a694c0fbb0339060ade2d9572
Hadi Diab <hadiibrahimdiab@gmail.com>
2026-02-02 10:38:38 +0200

    feat: add AboutSection component and integrate it into HomePage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
+import AboutSection from "../components/AboutSection/AboutSection";
 import HeroSection from "../components/HeroSection/HeroSection";
 
 export default function HomePage() {
     return (
         <>
             <HeroSection />
+            <AboutSection />
         </>
     )
 }
---
+++ src/components/AboutSection/AboutSection.jsx
@@ -0,0 +1,11 @@
+"use client"
+import './AboutSection.scss'
+
+
+export default function AboutSection() {
+    
+    return (
+        <>
+        </>
+    )
+}
