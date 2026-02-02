97151498e5e43e5233756df85d6bfab52b805bfe
Hadi Diab <hadiibrahimdiab@gmail.com>
2026-02-02 10:58:41 +0200

    feat: fetch and display portfolio data in HeroSection component

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,18 +1,43 @@
+"use client"
+
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import './HeroSection.scss'
 import Link from 'next/link'
 import { FiMail } from 'react-icons/fi'
 import { SiGithub, SiLinkedin } from 'react-icons/si'
+import { getPortfolioData } from '../../utils/data'
 
 export default function HeroSection() {
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        async function loadData() {
+            const portfolioData = await getPortfolioData()
+            setData(portfolioData)
+        }
+        loadData()
+    }, [])
+
+    if (!data) {
+        return (
+            <section className="heroContainer">
+                <div className="heroContant">
+                    <p>Loading...</p>
+                </div>
+            </section>
+        )
+    }
+
     return (
         <>
             <section className="heroContainer">
-                
                 <div className="heroContant">
-                    <p>Hi, I am</p>
-                    <h1 className="heroTitle"><span className="hightlightName">Hadi</span> Diab</h1>
-                    <p>Front-end Developer / UI Designer</p>
+                    <p>{data.personal.tagline}</p>
+                    <h1 className="heroTitle">
+                        <span className="hightlightName">{data.personal.name.split(' ')[0]}</span> {data.personal.name.split(' ')[1]}
+                    </h1>
+                    <p>{data.personal.title}</p>
 
                     <div className="heroButtons">
                         {/* <a href="#about" className="ctaOutline" aria-label="About me">ABOUT ME</a>
@@ -20,14 +45,13 @@ export default function HeroSection() {
                     </div>
 
                     <div className="socials" aria-label="Social links">
-                        <a className="icon" href="mailto:you@example.com" aria-label="Email"><FiMail size={18} /></a>
-                        <a className="icon" href="https://github.com" target="_blank" rel="noreferrer" aria-label="GitHub"><SiGithub size={18} /></a>
-                        <a className="icon" href="https://linkedin.com" target="_blank" rel="noreferrer" aria-label="LinkedIn"><SiLinkedin size={18} /></a>
+                        <a className="icon" href={data.socialLinks.email} aria-label="Email"><FiMail size={18} /></a>
+                        <a className="icon" href={data.socialLinks.github} target="_blank" rel="noreferrer" aria-label="GitHub"><SiGithub size={18} /></a>
+                        <a className="icon" href={data.socialLinks.linkedin} target="_blank" rel="noreferrer" aria-label="LinkedIn"><SiLinkedin size={18} /></a>
                     </div>
                 </div>
                 <div className="heroImage">
                     <div className="portrait">
-                        {/* Place your hero portrait in `public/hero.jpg` (or change the path below) */}
                         <Image src="/hero-Image.png" alt="Hero" fill style={{ objectFit: 'contain' }} />
                     </div>
                 </div>
@@ -37,11 +61,11 @@ export default function HeroSection() {
                     <div className="featureContent">
                         <h2>IT BERRIES</h2>
                         <p>
-                            Nulla in velit a metus rhoncus tempus. Nulla congue nulla vel sem varius finibus. Sed ornare sit amet lorem sed viverra. In vel urna quis libero viverra facilisis ut ac est. Morbi commodo, eros in dignissim tempus, lacus odio rutrum augue, in semper sem magna quis tellus.
+                            {data.personal.bio}
                         </p>
 
-                        <a className="readMore" href="#" aria-label="Read more">
-                            READ MORE
+                        <a className="readMore" href={data.resume.path} target="_blank" aria-label="Download Resume">
+                            DOWNLOAD CV
                         </a>
                     </div>
                 </div>
