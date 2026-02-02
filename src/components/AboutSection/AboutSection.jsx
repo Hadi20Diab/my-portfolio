a087876cae46459ad08ed799ae4e69455812996e
Hadi Diab <hadiibrahimdiab@gmail.com>
2026-02-02 14:34:29 +0200

    fix: remove DecorativeDivider from AboutSection and adjust padding for consistency

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -71,8 +71,6 @@ export default function AboutSection() {
                         )
                     })}
                 </div>
-
-                <DecorativeDivider />
             </div>
         </section>
     )
